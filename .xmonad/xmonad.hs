80b082790920e79d26102bb45ca64d42297fbbcf
sebastianuskh <sebastianus.kurniawan@gmail.com>
2020-10-04 09:20:59 +0700

    add xmobar & xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,7 +7,18 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 
-main = xmonad desktopConfig
-        { terminal  = "st"
-        , modMask   = mod4Mask
-        }
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ docks defaultConfig
+        { layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , terminal = "st"
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
