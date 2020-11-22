41a877337d826cd6c3425b3e49f0dabe4e178183
sebastianuskh <sebastianus.kurniawan@gmail.com>
2020-11-22 21:25:54 +0700

    made second screen not the main

--- .screenlayout/refresh.sh
+++ .screenlayout/refresh.sh
@@ -6,7 +6,7 @@ HDMI_STATUS=$(</sys/class/drm/card0-HDMI-A-1/status )
 
 if [ "connected" == "$HDMI_STATUS" ]; then
     xrandr --output eDP1 --mode 1600x900 --pos 1920x0 --rotate normal
-    xrandr --output HDMI1 --auto --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output HDMI1 --auto --mode 1920x1080 --pos 0x0 --rotate normal
 else
     xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 fi
