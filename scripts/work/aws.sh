12bd61babf6bf9b782885ef088a39afa20936864
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-06-26 17:40:24 +0700

    add remove unused work script

--- scripts/work/aws.sh
+++
@@ -1,3 +0,0 @@
-function runningec2fzf () {
-    aws ec2 describe-instances --output text --filters Name=instance-state-name,Values=running --query "Reservations[].Instances[].[InstanceId,PrivateIpAddress,Tags[?Key=='Name']|[0].Value]" | fzf
-}
