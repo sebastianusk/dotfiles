d9d362cebd82e17d9ffb893feb2e8b731641d7fd
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-06-27 09:09:44 +0700

    add config for fzf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,4 +10,16 @@ set -gx VISUAL 'nvim'
 set -gx PAGER 'bat'
 set -gx BROWSER 'vivaldi-stable'
 
+set -gx FZF_DEFAULT_OPTS "
+    --layout=reverse
+    --info=inline
+    --height=70%
+    --preview-window=:hidden
+    --preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+    --preview-window=right:60%:wrap
+    --bind '?:toggle-preview'
+"
+set -gx FZF_DEFAULT_COMMAND "rg --files --hidden"
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+
 fish_add_path -p $GOPATH/bin $HOME/.local/bin $HOME/.krew/bin
