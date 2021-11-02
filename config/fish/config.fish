477ffa8963762858d5cd1cd4472a10fbadd5f39c
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-11-02 11:49:39 +0700

    turn on vi mode

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -26,3 +26,5 @@ set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 fish_add_path -p $GOPATH/bin $HOME/.local/bin $HOME/.krew/bin /usr/lib/node_modules/.bin $HOME/.pub-cache/bin $HOME/.deno/bin
 
 source /opt/asdf-vm/asdf.fish
+
+fish_vi_key_bindings
