ab182f9bc81a28e7683a814f411d986698db7c0b
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2022-09-17 23:24:15 +0800

    add new brew, remove read alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -29,4 +29,3 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 fish_add_path -p $GOPATH/bin $HOME/.local/bin $HOME/.krew/bin /usr/lib/node_modules/.bin $HOME/.pub-cache/bin $HOME/.deno/bin /Applications/Pritunl.app/Contents/Resources
 
 fish_vi_key_bindings
-source ~/dotfiles/.aliasrc
