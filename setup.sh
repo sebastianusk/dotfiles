d90e41b07e01c8973fda5c941207368acd3341d9
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2023-01-02 20:07:04 +0800

    add logic not to apply ln on macs if not macs

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ ln -sfn ~/dotfiles/config/fish/conf.d ~/.config/fish/conf.d
 ln -sfn ~/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
 ln -sfn ~/dotfiles/config/fish/fish_plugins ~/.config/fish/fish_plugins
 ln -sfn ~/dotfiles/config/tmuxinator ~/.tmuxinator
-
 ln -sfn ~/dotfiles/config/k9s ~/.config/k9s
-ln -sfn ~/dotfiles/config/karabiner ~/.config/karabiner
-ln -sfn ~/dotfiles/config/karabiner.edn ~/.config/karabiner.edn
 ln -sfn ~/dotfiles/ctags.d ~/.ctags.d
-ln -sfn ~/dotfiles/Brewfile ~/Brewfile
-ln -sfn ~/dotfiles/Brewfile.lock.json ~/Brewfile.lock.json
-ln -sfn ~/dotfiles/amethyst.yml ~/.amethyst.yml
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  ln -sfn ~/dotfiles/config/karabiner ~/.config/karabiner
+  ln -sfn ~/dotfiles/config/karabiner.edn ~/.config/karabiner.edn
+  ln -sfn ~/dotfiles/Brewfile ~/Brewfile
+  ln -sfn ~/dotfiles/Brewfile.lock.json ~/Brewfile.lock.json
+  ln -sfn ~/dotfiles/amethyst.yml ~/.amethyst.yml
+fi
