7185daacc8ec67a93f47b4964e322c9ebd4798d3
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2023-01-02 20:04:42 +0800

    change fish to only copy needed

--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@ ln -sfn ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sfn ~/dotfiles/.tmux.conf.local ~/.tmux.conf.local
 ln -sfn ~/dotfiles/config/nvim ~/.config/nvim
 ln -sfn ~/dotfiles/config/alacritty.yml ~/.config/alacritty.yml
-ln -sfn ~/dotfiles/config/fish ~/.config/fish
-ln -sfn ~/dotfiles/config/k9s ~/.config/k9s
+ln -sfn ~/dotfiles/config/fish/conf.d ~/.config/fish/conf.d/
+ln -sfn ~/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
+ln -sfn ~/dotfiles/config/fish/fish_plugins ~/.config/fish/fish_plugins
 ln -sfn ~/dotfiles/config/tmuxinator ~/.tmuxinator
+
+ln -sfn ~/dotfiles/config/k9s ~/.config/k9s
 ln -sfn ~/dotfiles/config/karabiner ~/.config/karabiner
 ln -sfn ~/dotfiles/config/karabiner.edn ~/.config/karabiner.edn
 ln -sfn ~/dotfiles/ctags.d ~/.ctags.d
