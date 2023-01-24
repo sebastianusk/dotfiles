39a70c9005af1136298e6adc4e01a243ab93ed16
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2023-01-24 21:58:32 +0800

    add cd to root git

--- config/fish/funct.fish
+++ config/fish/funct.fish
@@ -68,3 +68,10 @@ function vstatic -d 'Connect to a database using vault static role (admin)'
 
     pgcli $connstring
 end
+
+function cdg
+  set -lx TOPLEVEL (git rev-parse --show-toplevel)
+    if test $status -eq 0
+      cd $TOPLEVEL
+  end
+end
