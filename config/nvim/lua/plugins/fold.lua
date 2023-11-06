3973e3ed9ad78409410581b1a078fecafb414069
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2023-11-06 23:59:50 +0800

    add fold to start on BufEnter

--- config/nvim/lua/plugins/fold.lua
+++ config/nvim/lua/plugins/fold.lua
@@ -17,6 +17,7 @@ return {
       desc = "Close All Folds",
     },
   },
+  event = "BufEnter",
   config = function()
     vim.o.foldcolumn = "1" -- '0' is not bad
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
