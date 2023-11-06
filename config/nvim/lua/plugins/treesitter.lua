2d484e784472fb2ef67cdbac9412ce7a92eb5728
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2023-11-06 23:28:43 +0800

    add ufo plugins for folding

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,13 +4,6 @@ return {
     build = ":TSUpdate",
     config = function()
       local configs = require("nvim-treesitter.configs")
-
-      vim.cmd([[
-      set foldmethod=expr
-      set foldexpr=nvim_treesitter#foldexpr()
-      set nofoldenable                     " Disable folding at startup.
-      ]])
-
       configs.setup({
         sync_install = false,
         highlight = { enable = true },
