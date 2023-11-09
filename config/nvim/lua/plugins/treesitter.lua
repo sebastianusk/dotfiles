d7824791dcc5a0024ee7ecdf0dac8211820a2027
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2023-11-09 09:58:00 +0800

    add treesitter text object

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -11,5 +11,59 @@ return {
       })
     end,
   },
-  { "nvim-treesitter/nvim-treesitter-context", opts = {} },
+  { "nvim-treesitter/nvim-treesitter-context", opts = {}, dependencies = { "nvim-treesitter/nvim-treesitter" } },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = { query = "@function.outer", desc = "Select around function" },
+              ["if"] = { query = "@function.inner", desc = "Select inner function" },
+              ["ac"] = { query = "@class.outer", desc = "Select around class" },
+              ["ic"] = { query = "@class.inner", desc = "Select inner class" },
+              ["at"] = { query = "@statement.outer", desc = "Select around statement" },
+              ["ab"] = { query = "@block.outer", desc = "Select around block" },
+              ["ib"] = { query = "@block.inner", desc = "Select around block" },
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+              ["]o"] = "@loop.*",
+              ["]k"] = "@scope",
+              ["]z"] = "@fold",
+              ["]t"] = "@statement.outer",
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+              ["[t"] = "@statement.outer",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+            goto_next = {
+              ["]d"] = "@conditional.outer",
+            },
+            goto_previous = {
+              ["[d"] = "@conditional.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
 }
