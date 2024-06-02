94127c816eb4374b3e79c0ba10fdff2719e24e59
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2024-06-02 11:18:40 +0800

    add pritunl cli to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -20,7 +20,7 @@ set -gx LC_CTYPE 'en_US.UTF-8'
 set -gx LC_ALL 'en_US.UTF-8'
 set -gx DOCKER_DEFAULT_PLATFORM linux/amd64
 
-fish_add_path -p $GOPATH/bin $HOME/.local/bin $HOME/.krew/bin /usr/lib/node_modules/.bin $HOME/.pub-cache/bin $HOME/.cargo/bin /opt/homebrew/opt/mysql-client/bin $HOME/.local/share/nvim/mason/bin $HOME/dotfiles/bin /opt/homebrew/sbin
+fish_add_path -p $GOPATH/bin $HOME/.local/bin $HOME/.krew/bin /usr/lib/node_modules/.bin $HOME/.pub-cache/bin $HOME/.cargo/bin /opt/homebrew/opt/mysql-client/bin $HOME/.local/share/nvim/mason/bin $HOME/dotfiles/bin /opt/homebrew/sbin /Applications/Pritunl.app/Contents/Resources
 
 if type -q brew
     fish_add_path /opt/homebrew/bin
