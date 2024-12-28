26aadf6b63f5e30f201487ead9b455a4bf36a6cd
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2024-12-28 09:16:09 +0700

    add the docker default platform config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,7 +18,7 @@ set -gx SHELL (which fish)
 set -gx DISPLAY ':0.0'
 set -gx LC_CTYPE 'en_US.UTF-8'
 set -gx LC_ALL 'en_US.UTF-8'
-set -gx DOCKER_DEFAULT_PLATFORM linux/amd64
+set -gx DOCKER_DEFAULT_PLATFORM linux/arm64
 
 fish_add_path -p $GOPATH/bin $HOME/.local/bin $HOME/.krew/bin /usr/lib/node_modules/.bin $HOME/.pub-cache/bin $HOME/.cargo/bin /opt/homebrew/opt/mysql-client/bin $HOME/.local/share/nvim/mason/bin $HOME/dotfiles/bin /opt/homebrew/sbin /Applications/Pritunl.app/Contents/Resources
 
