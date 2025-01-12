b5f710d43af9b4150f06e507881f4039b427de43
sebastianusk <sebastianus.kurniawan@gmail.com>
2025-01-12 23:02:15 +0800

    modify home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -1,36 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # TODO please change the username & home directory to your own
   home.username = "seb";
   home.homeDirectory = "/home/seb";
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # set cursor size and dpi for 4k monitor
-  xresources.properties = {
-    "Xcursor.size" = 16;
-    "Xft.dpi" = 172;
-  };
-
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
-
     neofetch
 
     # archives
@@ -105,7 +80,6 @@
     python3
   ];
 
-  # basic configuration of git, please change to your own
   programs.git = {
     enable = true;
     userName = "sebastianusk";
@@ -129,17 +103,12 @@
 
     vimAlias = true;
     vimdiffAlias = true;
-
-    plugins = with pkgs.vimPlugins; [
-      lazy-nvim
-    ];
-
-    extraConfig = ''
-      luafile ${../config/nvim/init.lua}
-    '';
   };
 
   home.file = {
+    ".config/nvim/init.lua" = {
+      source = ../config/nvim/init.lua;
+    };
     ".config/nvim/lua" = {
       source = ../config/nvim/lua;
       recursive = true;
