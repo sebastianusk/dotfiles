0dca711ebd5a02b03cc50c71ae5a3d3a38b678eb
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2025-06-22 20:19:16 +0800

    modify ai & codeium

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -10,7 +10,6 @@ return {
       endpoint = "https://api.openai.com/v1",
       model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
       timeout = 30000, -- timeout in milliseconds
-      temperature = 0, -- adjust if needed
       max_tokens = 4096,
       -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
     },
--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,13 +1,11 @@
 return {
   {
-    "Exafunction/codeium.nvim",
+    "Exafunction/windsurf.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "hrsh7th/nvim-cmp",
     },
-    config = function()
-      require("codeium").setup({})
-    end,
+    event = "BufEnter",
   },
   {
     "L3MON4D3/LuaSnip",
