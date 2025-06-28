6afdb64c7d324214ef4a107a869b6020fd9a8167
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2025-06-28 09:43:59 +0700

    update the secret fish and setup to sync the secret

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 ln -sfn ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sfn ~/dotfiles/.tmux.conf.local ~/.tmux.conf.local
+ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sfn ~/dotfiles/config/nvim ~/.config/nvim
+ln -sfn ~/dotfiles/config/mcphub ~/.config/mcphub
 ln -sfn ~/dotfiles/config/alacritty ~/.config/alacritty
 
 mkdir -p ~/.config/fish
@@ -10,6 +11,9 @@ ln -sfn ~/dotfiles/config/fish/fish_plugins ~/.config/fish/fish_plugins
 ln -sfn ~/dotfiles/config/fish/alias.fish ~/.config/fish/alias.fish
 ln -sfn ~/dotfiles/config/fish/funct.fish ~/.config/fish/funct.fish
 ln -sfn ~/dotfiles/config/fish/opts.fish ~/.config/fish/opts.fish
+cp ~/dotfiles/config/fish/secret.fish.example ~/dotfiles/config/fish/secret.fish
+ln -sfn ~/dotfiles/config/fish/secret.fish ~/.config/fish/secret.fish
+
 
 ln -sfn ~/dotfiles/config/tmuxinator ~/.tmuxinator
 ln -sfn ~/dotfiles/config/k9s ~/.config/k9s
@@ -20,6 +24,7 @@ ln -sfn ~/dotfiles/config/starship.toml ~/.config/starship.toml
 ln -sfn ~/dotfiles/.tool-versions ~/.tool-versions
 ln -sfn ~/dotfiles/.default-npm-packages ~/.default-npm-packages
 
+mkdir -p /usr/local/bin/
 sudo ln -sfn $(which fish) /usr/local/bin/fish
 sudo ln -sfn $(which tmux) /usr/local/bin/tmux
 
@@ -29,6 +34,13 @@ if [[ $OSTYPE == 'darwin'* ]]; then
   ln -sfn ~/dotfiles/Brewfile ~/Brewfile
   ln -sfn ~/dotfiles/Brewfile.lock.json ~/Brewfile.lock.json
   ln -sfn ~/dotfiles/amethyst.yml ~/.amethyst.yml
-  ln -sfn ~/dotfiles/amethyst.yml ~/.config/amethyst/amethyst.yml
-  sudo ln -sfn /opt/homebrew/bin/reattach-to-user-namespace /usr/local/bin/reattach-to-user-namespace
 fi
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || echo "Node.js plugin already added or failed to add."
+asdf plugin add python https://github.com/asdf-community/asdf-python.git || echo "Python plugin already added or failed to add."
+asdf plugin add flutter https://github.com/oerdnj/asdf-flutter.git || echo "Flutter plugin already added or failed to add."
+asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git || echo "Terraform plugin already added or failed to add."
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
