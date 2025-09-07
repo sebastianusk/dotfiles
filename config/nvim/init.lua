7d33c389ad4bf3a50fc40f9f12629722dd54886f
Sebastianus Kurniawan <sebastianus.kurniawan@gmail.com>
2025-09-07 10:34:47 +0800

    add auto read for nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,6 +3,12 @@ require("keymap")
 require("function")
 require("filetypes")
 
+-- Enhanced autoread - makes autoread actually work with external file changes
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
