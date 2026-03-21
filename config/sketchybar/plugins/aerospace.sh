3a86770e53e6be01c7bbc201ac0000953b898017
Sebastianus Kurniawan <sebastianus.kurniawan@dkatalis.com>
2026-03-21 09:40:57 +0800

    update aerospace and sketcybar to have it per monitor

--- config/sketchybar/plugins/aerospace.sh
+++ config/sketchybar/plugins/aerospace.sh
@@ -1,10 +1,41 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+WS="$1"
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+# Use the item name that sketchybar provides ($NAME)
+# This is the correct item to update regardless of how it's named
+
+# Highlight if this workspace is focused (change background color)
+if [ "$WS" = "$FOCUSED_WORKSPACE" ]; then
+	sketchybar --set "$NAME" background.color=0x44ffffff
 else
-    sketchybar --set $NAME background.drawing=off
+	sketchybar --set "$NAME" background.color=0x15ffffff
 fi
+
+# Determine workspace icon based on windows
+WINDOWS=$(aerospace list-windows --workspace "$WS" --json 2>/dev/null)
+COUNT=$(echo "$WINDOWS" | jq 'length' 2>/dev/null || echo "0")
+
+# Pad all icons to 2 characters for uniform width
+if [ "$COUNT" -eq 0 ]; then
+	ICON="○ "
+elif [ "$COUNT" -eq 1 ]; then
+	APP=$(echo "$WINDOWS" | jq -r '.[0]["app-name"]' 2>/dev/null)
+	case "$APP" in
+	*Google\ Chrome*) ICON=" " ;;
+	*Alacritty*) ICON=" " ;;
+	*Slack*) ICON=" " ;;
+	*Obsidian*) ICON=" " ;;
+	*Calendar*) ICON=" " ;;
+	*Mail*) ICON=" " ;;
+	*) ICON="● " ;;
+	esac
+else
+	if [ "$COUNT" -lt 10 ]; then
+		ICON="$COUNT "
+	else
+		ICON="$COUNT"
+	fi
+fi
+
+sketchybar --set "$NAME" icon="$ICON"
--- config/sketchybar/sync.sh
+++ config/sketchybar/sync.sh
@@ -1,1 +1,2 @@
 ln -sfn ~/dotfiles/config/sketchybar ~/.config/sketchybar
+brew services start sketchybar
